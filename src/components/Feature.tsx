bf407c71f2ca331ddb656f60eda18a692dad0cd0
AbdulSamad94 <abdulsamadsiddiqui2000@gmail.com>
2024-10-26 11:12:36 -0700

    added swiper

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -1,14 +1,29 @@
+"use client";
+
 import posts from "@/data/posts.json";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
 
 const Feature = () => {
   return (
-    <div>
-      <div>
-        {posts.slice(0, 6).map((item, index) => (
-          <div key={index}>{item.author}</div>
+    <section>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+      >
+        {posts.slice(0, 6).map((item) => (
+          <SwiperSlide key={item.id}>
+            <div>
+              <div>{item.author}</div>
+              <h2>{item.content}</h2>
+            </div>
+          </SwiperSlide>
         ))}
-      </div>
-    </div>
+      </Swiper>
+    </section>
   );
 };
 
