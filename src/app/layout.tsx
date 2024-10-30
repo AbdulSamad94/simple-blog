764fe4a8d560e4d7e1e7a4b499ffe4d26362b3c2
AbdulSamad94 <abdulsamadsiddiqui2000@gmail.com>
2024-10-30 23:39:13 +0500

    project done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
@@ -20,6 +21,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
