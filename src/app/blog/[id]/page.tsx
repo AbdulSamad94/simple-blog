1512ddc98f18a66f0c3ec6628b9316f0fe1fb2bd
AbdulSamad94 <abdulsamadsiddiqui2000@gmail.com>
2024-10-30 22:52:22 +0500

    bug fixed

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -51,7 +51,6 @@ const Page = ({ params }: { params: Promise<{ id: string }> }) => {
   const [user, setUser] = useState("");
   const [commentContent, setCommentContent] = useState("");
 
-  const data: string = new Date().getMonth.toString();
   if (!postDetail) {
     return <p className="text-center text-red-600">Post not found</p>;
   }
@@ -109,7 +108,7 @@ const Page = ({ params }: { params: Promise<{ id: string }> }) => {
           <h1 className="text-4xl font-bold">What is Lorem Ipsum?</h1>
           <p className="text-2xl mt-8">
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
+            industry. Lorem Ipsum has been the industrys standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book. It has survived not
             only five centuries, but also the leap into electronic typesetting,
@@ -126,13 +125,13 @@ const Page = ({ params }: { params: Promise<{ id: string }> }) => {
                 It is a long established fact that a reader will be distracted
                 by the readable content of a page when looking at its layout.
                 The point of using Lorem Ipsum is that it has a more-or-less
-                normal distribution of letters, as opposed to using 'Content
-                here, content here', making it look like readable English. Many
+                normal distribution of letters, as opposed to using Content
+                here, content here, making it look like readable English. Many
                 desktop publishing packages and web page editors now use Lorem
-                Ipsum as their default model text, and a search for 'lorem
-                ipsum' will uncover many web sites still in their infancy.
-                Various versions have evolved over the years, sometimes by
-                accident, sometimes on purpose (injected humour and the like).
+                Ipsum as their default model text, and a search for lorem ipsum
+                will uncover many web sites still in their infancy. Various
+                versions have evolved over the years, sometimes by accident,
+                sometimes on purpose (injected humour and the like).
               </p>
             </div>
           </div>
@@ -145,16 +144,16 @@ const Page = ({ params }: { params: Promise<{ id: string }> }) => {
             obscure Latin words, consectetur, from a Lorem Ipsum passage, and
             going through the cites of the word in classical literature,
             discovered the undoubtable source. Lorem Ipsum comes from sections
-            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
+            1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes
             of Good and Evil) by Cicero, written in 45 BC. This book is a
             treatise on the theory of ethics, very popular during the
-            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
-            amet..", comes from a line in section 1.10.32.
+            Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit
+            amet.., comes from a line in section 1.10.32.
           </p>
           <p className="text-2xl mt-8">
             The standard chunk of Lorem Ipsum used since the 1500s is reproduced
-            below for those interested. Sections 1.10.32 and 1.10.33 from "de
-            Finibus Bonorum et Malorum" by Cicero are also reproduced in their
+            below for those interested. Sections 1.10.32 and 1.10.33 from de
+            Finibus Bonorum et Malorum by Cicero are also reproduced in their
             exact original form, accompanied by English versions from the 1914
             translation by H. Rackham.
           </p>
